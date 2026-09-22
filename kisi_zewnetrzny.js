213d8883062e0239f87498d1581c4b54e2ee5594
eisenkisielof-byte <eisenkisielof@gmail.com>
2026-09-22 18:32:47 +0200

    Create kisi_zewnetrzny.js

---
+++ kisi_zewnetrzny.js
@@ -0,0 +1,7 @@
+document.write("ostatnia modyfikacja strony".fontcolor("olive").bold().fontsize(5) .strike()+"<br>"); 
+document.write(document.lastModified).fontcolor("lime").fontsize(5)+"<br>"); 
+
+
+
+
+
