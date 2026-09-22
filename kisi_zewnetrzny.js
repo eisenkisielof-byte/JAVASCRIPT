b38c39755bdf3af5e433b7acf959d08b2cc31602
eisenkisielof-byte <eisenkisielof@gmail.com>
2026-09-22 18:32:10 +0200

    Delete kisi_zewnetrzny.js

--- kisi_zewnetrzny.js
+++
@@ -1,7 +0,0 @@
-document.write("ostatnia modyfikacja strony".fontcolor("olive").bold().fontsize(5) .strike()+"<br>"); 
-document.write(document.lastModified).fontcolor("lime").fontsize(5)+"<br>"); 
-
-
-
-
-
